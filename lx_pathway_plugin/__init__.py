e7eb022b5ad50514c02f43d0da5ad2ae31e93ebd
Braden MacDonald <mail@bradenm.com>
2020-01-10 12:36:35 -0800

    Quick fix

--- lx_pathway_plugin/__init__.py
+++ lx_pathway_plugin/__init__.py
@@ -6,4 +6,4 @@ from __future__ import absolute_import, unicode_literals
 
 __version__ = '0.1.1'
 
-default_app_config = 'lx_pathway_plugin.apps.LxModulestoreExporterAppConfig'  # pylint: disable=invalid-name
+default_app_config = 'lx_pathway_plugin.apps.LxPathwayPluginAppConfig'  # pylint: disable=invalid-name
