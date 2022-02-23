3b135d237b625a7f77759cc2976c18aa5966a6e6
Jillian Vogel <jill@opencraft.com>
2022-02-23 16:49:45 +1030

    refactor: for use inside the labxchange app
    * uses labxchange learning contexts where available, instead of openedx
    * adds a simple AppConfig with no dependencies on open edx,
      so the plugin can behave like a normal app within LabXchange

--- lx_pathway_plugin/apps.py
+++ lx_pathway_plugin/apps.py
@@ -2,42 +2,9 @@
 """
 lx_pathway_plugin Django application initialization.
 """
-from django.apps import AppConfig
-from openedx.core.djangoapps.plugins.constants import PluginSettings, PluginURLs, ProjectType, SettingsType
-
-
-class LxPathwayPluginAppConfig(AppConfig):
-    """
-    Configuration for the lx_pathway_plugin Django plugin application.
-
-    See: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
-    """
-
-    name = 'lx_pathway_plugin'
-    plugin_app = {
-        PluginURLs.CONFIG: {
-            ProjectType.CMS: {
-                # The namespace to provide to django's urls.include.
-                PluginURLs.NAMESPACE: 'lx_pathway_plugin',
-            },
-            ProjectType.LMS: {
-                # The namespace to provide to django's urls.include.
-                PluginURLs.NAMESPACE: 'lx_pathway_plugin',
-                # use a different set of URLs/views in the LMS
-                PluginURLs.RELATIVE_PATH: 'urls_lms',
-            },
-        },
-        PluginSettings.CONFIG: {
-            ProjectType.CMS: {
-                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: 'settings'},
-            },
-            ProjectType.LMS: {
-                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: 'settings'},
-            },
-        },
-    }
-
-    def ready(self):
-        """
-        Load signal handlers when the app is ready.
-        """
+try:
+    # Use the Open edX app config if we can
+    from .openedx_apps import LxPathwayPluginAppConfig
+except ModuleNotFoundError:
+    # Otherwise, fall back to the LabXchange app config
+    from .lx_apps import LxPathwayPluginAppConfig
