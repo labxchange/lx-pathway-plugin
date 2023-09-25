ec6778e51b0b56056669c43c32f1e6c45f97359f
Pooja Kulkarni <pooja@opencraft.com>
2023-09-25 10:26:39 -0400

    depr: remove db table and it's reference

--- lx_pathway_plugin/models.py
+++ lx_pathway_plugin/models.py
@@ -3,14 +3,9 @@ Django models for the pathways plugin.
 """
 # pylint: disable=abstract-method
 import random
-import uuid
 from logging import getLogger
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
-from django.db import models
-from jsonfield.fields import JSONField
-from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import UsageKey
 from rest_framework import serializers
 
@@ -20,76 +15,6 @@ log = getLogger(__name__)
 User = get_user_model()
 
 
-class Pathway(models.Model):
-    """
-    Model for representing a LabXchange pathway
-    """
-    # edX models are required to have an integer primary key; we don't use it.
-    id = models.AutoField(primary_key=True)
-    # UUID of the pathway
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # Every pathway is owned either by a user or by a group
-    owner_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    owner_group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.CASCADE)
-    # The actual pathway data (draft and published version)
-    # Contains the list of items, original XBlock IDs, notes, etc.
-    # The format of these *_data fields is defined and enforced by PathwayDataSerializer (see below)
-    draft_data = JSONField(null=False, blank=True, default=dict)
-    published_data = JSONField(null=False, blank=True, default=dict)
-
-    def __str__(self):
-        return "<Pathway: {uuid} >".format(uuid=self.uuid)  # xss-lint: disable=python-wrap-html
-
-    @property
-    def title(self):
-        """
-        Get the title of this pathway.
-        """
-        title = self.published_data.get("title", "")
-        if not title:
-            title = self.draft_data.get("title", "")
-        if not title:
-            title = "Pathway {}".format(self.uuid)
-        return title
-
-    @property
-    def key(self):
-        """
-        Get the opaque key (LearningContextKey) for this pathway
-        """
-        return PathwayLocator(uuid=self.uuid)
-
-    def save(self, *args, **kwargs):  # pylint: disable=arguments-differ
-        """
-        Validate and clean before saving.
-        """
-        # if both owner_user and owner_group are nonexistent or both are existing, error:
-        if (not self.owner_user) == (not self.owner_group):
-            # We can remove this check and replace it with a proper database constraint
-            # once Open edX is upgraded to Django 2.2+
-            raise serializers.ValidationError("One and only one of 'user' and 'group' must be set.")
-        for data_set in ('draft_data', 'published_data'):
-            serializer = PathwayDataSerializer(data=getattr(self, data_set))
-            serializer.is_valid(raise_exception=True)
-            items = serializer.validated_data["items"]
-            num_items = len(items)
-            if num_items > 100:
-                raise serializers.ValidationError("Too many items in the pathway.")
-            id_set = set(item["id"] for item in items)
-            if len(id_set) != num_items:
-                raise serializers.ValidationError("Some item IDs are not unique.")
-            for item in items:
-                try:
-                    UsageKey.from_string(item["original_usage_id"])
-                except InvalidKeyError as exc:
-                    raise serializers.ValidationError("Invalid item ID: {}".format(item["original_usage_id"])) from exc
-                if item.get("version") is not None:
-                    raise serializers.ValidationError("Pinning the version of pathway items is no longer supported.")
-                if "usage_id" in item:
-                    del item["usage_id"]  # This field is only added in the REST API response, never saved to DB
-            # Replace the current value of draft_data or published_data with the cleaned version
-            setattr(self, data_set, serializer.validated_data)
-        return super().save(*args, **kwargs)
 
 # Serializers
 
--- lx_pathway_plugin/urls.py
+++ lx_pathway_plugin/urls.py
@@ -1,16 +1,6 @@
 """
 Studio URL configuration for lx-pathway-plugin.
 """
-from django.conf.urls import include, url
-
-from . import views
 
 urlpatterns = [
-    url(r'^api/lx-pathways/v1/', include([
-        url(r'pathway/$', views.create_pathway),
-        # Get/edit/delete a specific pathway:
-        url(r'pathway/(?P<pathway_key_str>[^/]+)/$', views.PathwayView.as_view()),
-        # Publish or revert changes to a pathway:
-        url(r'pathway/(?P<pathway_key_str>[^/]+)/publish/$', views.PathwayPublishView.as_view()),
-    ])),
 ]
